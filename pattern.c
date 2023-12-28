6e256b18cbf775e2bf155a415af1bd8e7d9fd64a
Sriamirtha K <154916122+SriamirthaK@users.noreply.github.com>
2023-12-28 14:50:02 +0530

    pattern.c

--- pattern.c
+++ pattern.c
@@ -12,3 +12,54 @@ int main()
         printf("\n");
     }
 }
+
+/*and the output is enter num 5
+*
+**
+***
+****
+*****/
+
+#include<stdio.>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=1;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("1 ");
+        }
+        printf("\n");
+    }
+}
+
+/*and the output is enter num 5
+1
+11
+111
+1111
+11111*/
+
+#include<stdio.>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=1;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d",j+1);
+        }
+        printf("\n");
+    }
+}
+
+/*and the output is enter num 5
+1
+12
+123
+1234
+12345*/
